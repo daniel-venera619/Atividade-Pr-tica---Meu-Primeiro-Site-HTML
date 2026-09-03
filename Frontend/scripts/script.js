de0ba33f22b05bb399254d0d5788f216beab64d9
daniel-venera619 <venera619@gmail.com>
2026-09-02 21:40:14 -0300

    Botão do carrinho
    Foi adicionado um contador ao botão do carrinho. A cada vez que o usuário clica no botão "Adicionar ao carrinho", o contador é incrementado, indicando a quantidade de itens adicionados ao carrinho.

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -38,3 +38,56 @@ btnCancelar.addEventListener("click", function() {
     modal.style.display = "none";
 
 });
+
+//CONTADOR DO CARRINHO
+
+// Seleciona o botão do carrinho
+const carrinho = document.getElementById("carrinho");
+
+// Seleciona o contador
+const contadorCarrinho = document.getElementById("contador-carrinho");
+
+// Começa a quantidade em zero
+let quantidadeProdutos = 0;
+
+// Quando clicar no carrinho
+carrinho.addEventListener("click", function(event) {
+
+    // Impede o comportamento padrão do link
+    event.preventDefault();
+
+    // Aumenta a quantidade em 1
+    quantidadeProdutos++;
+
+    // Atualiza o número mostrado na tela
+    contadorCarrinho.textContent = quantidadeProdutos;
+
+});
+
+
+
+// BOTÃO DE ENVIAR MENSAGEM
+
+// Seleciona o formulário pelo ID
+const formulario = document.getElementById("formulario");
+
+// Seleciona a mensagem de sucesso pelo ID
+const mensagemSucesso = document.getElementById("mensagem-sucesso");
+
+// Detecta quando o formulário for enviado
+formulario.addEventListener("submit", function(event) {
+
+    // Impede que a página seja recarregada
+    event.preventDefault();
+
+    // Mostra a mensagem de sucesso
+    mensagemSucesso.textContent = "Mensagem enviada com sucesso!";
+
+    // Exibe a mensagem na tela
+    mensagemSucesso.style.display = "block";
+
+    // Limpa os campos do formulário
+    formulario.reset();
+
+});
+
